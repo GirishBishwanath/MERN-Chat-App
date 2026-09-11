db906bba9495aac504c992ea4fde640da66592a2
Girish Bishwanath <120439877+GirishBishwanath@users.noreply.github.com>
2026-09-12 03:36:12 +0530

    refactor(errors): remove superseded JavaScript application error

--- Backend/errors/AppError.js
+++
@@ -1,9 +0,0 @@
-export class AppError extends Error {
-  constructor(message, statusCode, code, details = undefined) {
-    super(message);
-    this.name = "AppError";
-    this.statusCode = statusCode;
-    this.code = code;
-    this.details = details;
-  }
-}
