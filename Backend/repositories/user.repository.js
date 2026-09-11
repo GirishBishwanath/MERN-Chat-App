647252e2cf17d1e59950136907ba7d8781503318
Girish Bishwanath <120439877+GirishBishwanath@users.noreply.github.com>
2026-09-12 03:06:59 +0530

    refactor(api): isolate user persistence

---
+++ Backend/repositories/user.repository.js
@@ -0,0 +1,14 @@
+import User from "../models/user.model.js";
+
+export const findByEmail = (email, includePassword = false) => {
+  const query = User.findOne({ email });
+  return includePassword ? query.select("+password") : query;
+};
+
+export const createUser = (data) => User.create(data);
+
+export const findPublicById = (id) =>
+  User.findById(id).select("_id fullname email");
+
+export const listExcept = (userId) =>
+  User.find({ _id: { $ne: userId } }).select("_id fullname email");
