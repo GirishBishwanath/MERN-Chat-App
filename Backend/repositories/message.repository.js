4ec0e851ac25c4525bd9bd5a25ea5c847c95f0f4
Girish Bishwanath <120439877+GirishBishwanath@users.noreply.github.com>
2026-09-12 03:38:49 +0530

    refactor(chat): remove superseded JavaScript message repository

--- Backend/repositories/message.repository.js
+++
@@ -1,6 +0,0 @@
-import Message from "../models/message.model.js";
-
-export const createMessage = (data) => Message.create(data);
-
-export const findMessagesByIds = (ids) =>
-  Message.find({ _id: { $in: ids } }).sort({ createdAt: 1 });
